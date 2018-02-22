d6efad6959fd324ae507028ee1a028990442529b
Seth Wax <sethlwax@gmail.com>
2018-02-22 10:36:14 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,10 +8,8 @@ WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5
 
 def won?(board)
   winning_combination = nil
-  x_places = board.select {|index| index == "X"}
-  o_places = board.select {|index| index == "O"}
   WIN_COMBINATIONS.each do |combination|
-    if combination.all? {|index| x_places.include?(index)} || combination.all? {|index| o_places.include?(index)}
+    if combination.all? {|index| board[index] == "X"} || combination.all? {|index| board[index] == "O"}
       winning_combination = combination
     end
   end
