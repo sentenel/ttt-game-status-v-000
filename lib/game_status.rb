f599462dbecd9504d1a6f05295850a460d7b09b4
Seth Wax <sethlwax@gmail.com>
2018-02-22 10:31:44 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,3 +5,15 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
+def won?(board)
+  winning_combination = nil
+  x_places = board.select {|index| index == "X"}
+  o_places = board.select {|index| index == "O"}
+  WIN_COMBINATIONS.each do |combination|
+    if combination.all? {|index| x_places.include?(index)} || combination.all? {|index| o_places.include?(index)}
+      winning_combination = combination
+    end
+  end
+  winning_combination
+end
